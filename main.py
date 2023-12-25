928dfacf78fc845e880ad55347a3ea79fdc8cf7c
asesorov <crafter2022@gmail.com>
2023-12-25 15:25:13 +0300

    Fix current found issues

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+
 st.title('Введите ид работника [0: 19], в ответ выведем 3 наиболее подходящие вакансии')
 st.subheader('С работниками и вакансиями можно ознакомиться в датасетах test_recs.csv и vacancies.csv соответственно')
 
 uid_to_vname = pd.read_csv('./data/uid_to_vname.csv')
-number = st.number_input("Введите ид работника", value=0, min_value = 0, max_value=19, step=1)
+number = st.number_input("Введите ид работника", value=0, min_value=0, max_value=19, step=1)
 vacs_to_u = uid_to_vname[uid_to_vname['id'] == number]['v_names'][number]
 st.text(vacs_to_u)
